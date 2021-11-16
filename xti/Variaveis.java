0f825aefe38d82a590fdab0f97196fc4275d28dd
igorfrancesco <igorfrancesco2@gmail.com>
2021-11-16 15:24:41 -0300

    Class de exemplo

--- xti/Variaveis.java
+++ xti/Variaveis.java
@@ -4,16 +4,14 @@ public class Variaveis {
 
     public static void main(String[] args) {
 
-        String nome = "Igor";
-        int idade = 29;
-        boolean casado = false;
+        // String nome = "Igor";
+        // int idade = 29;
+        // boolean casado = false;
 
-        byte b = 127;
-        short s = (short) 32768;
+        // byte b = 127;
+        // short s = (short) 32768;
 
         System.out.println("********************************");
-
-        Double preco = new Double (20.69);
 
     }
 
