231a799423ec2b418f1fdaad12bb8bb71da6c99a
igorfrancesco <igorfrancesco2@gmail.com>
2021-11-16 10:21:56 -0300

    xti- cursos

---
+++ xti/Variaveis.java
@@ -0,0 +1,14 @@
+package xti;
+
+public class Variaveis {
+
+    public static void main(String[] args) {
+
+        String nome = "Igor";
+        int idade = 29;
+        boolean casado = false;
+
+        System.out.println("Texto");
+    }
+
+}
